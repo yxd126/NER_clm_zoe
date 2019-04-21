083d71f29d5e0e425948f3a421265fe5ff854eaf
Xiaodong Yu <dd95126@gmail.com>
2019-04-21 15:19:19 -0500

    include ZOE

---
+++ scripts.py
@@ -0,0 +1,303 @@
+import os
+import pickle
+import sqlite3
+import sys
+
+from ccg_nlpy import local_pipeline
+
+from cache import SurfaceCache
+from main import ZoeRunner
+from zoe_utils import DataReader
+from zoe_utils import ElmoProcessor
+from zoe_utils import Sentence
+
+
+def convert_esa_map(esa_file_name, freq_file_name, invcount_file_name):
+    esa_map = {}
+    with open(esa_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            value = line.split("\t")[1]
+            esa_map[key] = value
+    with open('data/esa/esa.pickle', 'wb') as handle:
+        pickle.dump(esa_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    freq_map = {}
+    with open(freq_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            value = int(line.split("\t")[1])
+            freq_map[key] = value
+    with open('data/esa/freq.pickle', 'wb') as handle:
+        pickle.dump(freq_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    invcount_map = {}
+    with open(invcount_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            value = int(line.split("\t")[1])
+            invcount_map[key] = value
+    with open('data/esa/invcount.pickle', 'wb') as handle:
+        pickle.dump(invcount_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def convert_wikilinks_sent_examples(sent_example_file_name):
+    sent_example_map = {}
+    max_bytes = 2 ** 31 - 1
+    with open(sent_example_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            value = line.split("\t")[1]
+            sent_example_map[key] = value
+    bytes_out = pickle.dumps(sent_example_map, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('data/sent_example.pickle', 'wb') as handle:
+        for idx in range(0, len(bytes_out), max_bytes):
+            handle.write(bytes_out[idx:idx + max_bytes])
+
+
+def convert_freebase(freebase_file_name, freebase_sup_file_name):
+    ret_map = {}
+    with open(freebase_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            val = line.split("\t")[1]
+            ret_map[key] = val
+    with open(freebase_sup_file_name) as f:
+        for line in f:
+            line = line.strip()
+            if len(line.split("\t")) <= 1:
+                continue
+            key = line.split("\t")[0]
+            val = line.split("\t")[1]
+            if key not in ret_map:
+                ret_map[key] = val
+    with open('data/title2freebase.pickle', 'wb') as handle:
+        pickle.dump(ret_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def convert_prob(prob_file_name, n2c_fil2_name):
+    prob_map = {}
+    n2c_map = {}
+    with open(n2c_fil2_name) as f:
+        for line in f:
+            line = line.strip()
+            n2c_map[line.split("\t")[0]] = line.split("\t")[1]
+    with open(prob_file_name) as f:
+        for line in f:
+            line = line.strip()
+            key = line.split("\t")[0]
+            val = float(line.split("\t")[1])
+            surface = key.split("|")[0]
+            title = key.split("|")[1]
+            if title in n2c_map:
+                title = n2c_map[title]
+            if surface in prob_map:
+                cur_highest = prob_map[surface][1]
+                if val > cur_highest:
+                    prob_map[surface] = (title, val)
+            else:
+                prob_map[surface] = (title, val)
+    with open('data/prior_prob.pickle', 'wb') as handle:
+        pickle.dump(prob_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def convert_cached_embeddings(raw_file_name, output_file_name):
+    ret_map = {}
+    max_bytes = 2 ** 31 - 1
+    with open(raw_file_name) as f:
+        for line in f:
+            line = line.strip()
+            token = line.split("\t")[0]
+            if line.split("\t")[1] == "null":
+                continue
+            vals = line.split("\t")[1].split(",")
+            vec = []
+            for val in vals:
+                vec.append(float(val))
+            ret_map[token] = vec
+    bytes_out = pickle.dumps(ret_map, protocol=pickle.HIGHEST_PROTOCOL)
+    with open(output_file_name, 'wb') as handle:
+        for idx in range(0, len(bytes_out), max_bytes):
+            handle.write(bytes_out[idx:idx + max_bytes])
+
+
+def reduce_cache_file_size(cache_pickle_file_name, title_file_name, out_file_name):
+    with open(cache_pickle_file_name, "rb") as handle:
+        cache_map = pickle.load(handle)
+    title_set = set()
+    with open(title_file_name, "r") as f:
+        for line in f:
+            line = line.strip()
+            title_set.add(line)
+    ret_map = {}
+    for key in cache_map:
+        if key in title_set:
+            ret_map[key] = cache_map[key]
+    with open(out_file_name, "wb") as handle:
+        pickle.dump(ret_map, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def check_data_file_integrity(mode=""):
+    file_list = [
+        'data/esa/esa.pickle',
+        'data/esa/freq.pickle',
+        'data/esa/invcount.pickle',
+        'data/prior_prob.pickle',
+        'data/sent_example.pickle',
+        'data/title2freebase.pickle',
+    ]
+    corpus_supplements = []
+    if mode == "figer":
+        corpus_supplements = [
+            'data/FIGER/target.embedding.pickle',
+            'data/FIGER/wikilinks.embedding.pickle',
+            'mapping/figer.mapping',
+            'mapping/figer.logic.mapping'
+        ]
+    passed = True
+    for file in file_list + corpus_supplements:
+        if not os.path.isfile(file):
+            print("[ERROR]: Missing " + file)
+            passed = False
+    if not passed:
+        print("You have one or more file missing. Please refer to README for solution.")
+    else:
+        print("All required or suggested files are here. Go ahead and run experiments!")
+
+
+def compare_runlogs(runlog_file_a, runlog_file_b):
+    if not os.path.isfile(runlog_file_a) or not os.path.isfile(runlog_file_b):
+        print("Invalid input file names")
+    with open (runlog_file_a, "rb") as handle:
+        log_a = pickle.load(handle)
+    with open (runlog_file_b, "rb") as handle:
+        log_b = pickle.load(handle)
+    for sentence in log_a:
+        for compare_sentence in log_b:
+            if sentence.get_sent_str() == compare_sentence.get_sent_str():
+                if sentence.get_mention_surface() == compare_sentence.get_mention_surface():
+                    if sentence.predicted_types != compare_sentence.predicted_types:
+                        print(sentence.get_sent_str())
+                        print(sentence.get_mention_surface())
+                        print(sentence.gold_types)
+                        print("Log A prediction: " + str(sentence.predicted_types))
+                        print("Log B prediction: " + str(compare_sentence.predicted_types))
+
+
+def produce_cache():
+    elmo_processor = ElmoProcessor(allow_tensorflow=True)
+    to_process = []
+    to_process_concepts = []
+    sorted_pairs = sorted(elmo_processor.sent_example_map.items())
+    cur_processing_file_num = ord(sorted_pairs[0][0][0])
+    sub_map_index = 0
+    max_bytes = 2 ** 31 - 1
+    for pair in sorted_pairs:
+        concept = pair[0]
+        file_num = ord(concept[0])
+        if file_num != cur_processing_file_num or len(to_process) > 10000:
+            new_start = False
+            if file_num != cur_processing_file_num:
+                new_start = True
+            out_file_name = "data/cache/batch_" + str(cur_processing_file_num) + "_" + str(sub_map_index) + ".pickle"
+            if new_start:
+                out_file_name = "data/cache/batch_" + str(cur_processing_file_num) + ".pickle"
+            if not os.path.isfile(out_file_name) and cur_processing_file_num >= 65:
+                print("Prepared to run ELMo on " + chr(cur_processing_file_num))
+                print("This batch contains " + str(len(to_process_concepts)) + " concepts, and " + str(len(to_process)) + " sentences.")
+                elmo_map = elmo_processor.process_batch(to_process)
+                batch_map = {}
+                for processed_concept in to_process_concepts:
+                    if processed_concept in elmo_map:
+                        batch_map[processed_concept] = elmo_map[processed_concept]
+                bytes_out = pickle.dumps(batch_map, protocol=pickle.HIGHEST_PROTOCOL)
+                with open(out_file_name, "wb") as handle:
+                    for idx in range(0, len(bytes_out), max_bytes):
+                        handle.write(bytes_out[idx:idx + max_bytes])
+                print("Processed all concepts start with " + chr(cur_processing_file_num))
+                print()
+            to_process = []
+            to_process_concepts = []
+            if new_start:
+                cur_processing_file_num = file_num
+                sub_map_index = 0
+            else:
+                sub_map_index += 1
+        example_sentences_str = elmo_processor.sent_example_map[concept]
+        example_sentences = example_sentences_str.split("|||")
+        for i in range(0, min(len(example_sentences), 10)):
+            to_process.append(example_sentences[i])
+        to_process_concepts.append(concept)
+
+
+def progress_bar(value, endvalue, bar_length=20):
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length) - 1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+    sys.stdout.write("\rProgress: [{0}] {1}%".format(arrow + spaces, round(percent * 100, 3)))
+    sys.stdout.flush()
+
+
+def produce_surface_cache(db_name, cache_name):
+    pipeline = local_pipeline.LocalPipeline()
+    cache = SurfaceCache(db_name, server_mode=False)
+    runner = ZoeRunner()
+    runner.elmo_processor.load_sqlite_db(cache_name, server_mode=False)
+    dataset = DataReader("data/large_text.json", size=-1, unique=True)
+    counter = 0
+    total = len(dataset.sentences)
+    for sentence in dataset.sentences:
+        ta = pipeline.doc([sentence.tokens], pretokenized=True)
+        for chunk in ta.get_shallow_parse:
+            new_sentence = Sentence(sentence.tokens, chunk['start'], chunk['end'])
+            runner.process_sentence(new_sentence)
+            cache.insert_cache(new_sentence)
+        progress_bar(counter, total)
+
+
+def produce_magnitude_vec_file(db_name, out_file):
+    conn = sqlite3.connect(db_name)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM data")
+    w = open(out_file, "w")
+    for row in cursor:
+        key = row[0]
+        val = row[1]
+        val = val[1:-1].replace(",", "")
+        w.write(key + " " + val + "\n")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("[ERROR]: No command given.")
+        exit(0)
+    if sys.argv[1] == "CHECKFILE":
+        if len(sys.argv) == 2:
+            check_data_file_integrity()
+        else:
+            check_data_file_integrity(sys.argv[2])
+    if sys.argv[1] == "COMPARE":
+        if len(sys.argv) < 4:
+            print("Need two files for comparison.")
+        compare_runlogs(sys.argv[2], sys.argv[3])
+    if sys.argv[1] == "CACHE":
+        produce_cache()
+    if sys.argv[1] == "SURFACECACHE":
+        produce_surface_cache("data/surface_cache.db", "/Volumes/Storage/Resources/wikilinks/elmo_cache_correct.db")
+    if sys.argv[1] == "PRODUCE_VEC":
+        produce_magnitude_vec_file("/Volumes/External/elmo_cache_correct.db", "/Volumes/External/elmo_cache.vec")
---
+++ server.py
@@ -0,0 +1,312 @@
+import json
+import signal
+import time
+import traceback
+
+from ccg_nlpy import local_pipeline
+from flask import Flask
+from flask import request
+from flask import send_from_directory
+from flask_cors import CORS
+
+from cache import ServerCache
+from cache import SurfaceCache
+from main import ZoeRunner
+from zoe_utils import InferenceProcessor
+from zoe_utils import Sentence
+
+
+class Server:
+
+    """
+    Initialize the server with needed resources
+    @sql_db_path: The path pointing to the ELMo caches sqlite file
+    """
+    def __init__(self, sql_db_path, surface_cache_path):
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.mem_cache = ServerCache()
+        self.surface_cache = SurfaceCache(surface_cache_path)
+        self.pipeline = local_pipeline.LocalPipeline()
+        self.pipeline_initialize_helper(['.'])
+        self.runner = ZoeRunner(allow_tensorflow=True)
+        status = self.runner.elmo_processor.load_sqlite_db(sql_db_path, server_mode=True)
+        if not status:
+            print("ELMo cache file is not found. Server mode is prohibited without it.")
+            print("Please contact the author for this cache, or modify this code if you know what you are doing.")
+            exit(1)
+        self.runner.elmo_processor.rank_candidates_vec()
+        signal.signal(signal.SIGINT, self.grace_end)
+
+    @staticmethod
+    def handle_root(path):
+        return send_from_directory('./frontend', path)
+
+    @staticmethod
+    def handle_redirection():
+        return Server.handle_root("index.html")
+
+    def parse_custom_rules(self, rules):
+        type_to_titles = {}
+        freebase_freq_total = {}
+        for rule in rules:
+            title = rule.split("|||")[0]
+            freebase_types = []
+            if title in self.runner.inference_processor.freebase_map:
+                freebase_types = self.runner.inference_processor.freebase_map[title].split(",")
+            for ft in freebase_types:
+                if ft in freebase_freq_total:
+                    freebase_freq_total[ft] += 1
+                else:
+                    freebase_freq_total[ft] = 1
+            custom_type = rule.split("|||")[1]
+            if custom_type in type_to_titles:
+                type_to_titles[custom_type].append(title)
+            else:
+                type_to_titles[custom_type] = [title]
+        counter = 0
+        ret = {}
+        for custom_type in type_to_titles:
+            freebase_freq = {}
+            for title in type_to_titles[custom_type]:
+                freebase_types = []
+                if title in self.runner.inference_processor.freebase_map:
+                    freebase_types = self.runner.inference_processor.freebase_map[title].split(",")
+                    counter += 1
+                for freebase_type in freebase_types:
+                    if freebase_type in freebase_freq:
+                        freebase_freq[freebase_type] += 1
+                    else:
+                        freebase_freq[freebase_type] = 1
+            for ft in freebase_freq:
+                if float(freebase_freq[ft]) > float(counter) * 0.5 and freebase_freq[ft] == freebase_freq_total[ft]:
+                    ft = "/" + ft.replace(".", "/")
+                    ret[ft] = custom_type
+        return ret
+
+    """
+    Main request handler
+    It requires the request to contain required information like tokens/mentions
+    in the format of a json string
+    """
+    def handle_input(self):
+        start_time = time.time()
+        ret = {}
+        r = request.get_json()
+        if "tokens" not in r or "mention_starts" not in r or "mention_ends" not in r or "index" not in r:
+            ret["type"] = [["INVALID_INPUT"]]
+            ret["index"] = -1
+            ret["mentions"] = []
+            ret["candidates"] = [[]]
+            return json.dumps(ret)
+        sentences = []
+        for i in range(0, len(r["mention_starts"])):
+            sentence = Sentence(r["tokens"], int(r["mention_starts"][i]), int(r["mention_ends"][i]), "")
+            sentences.append(sentence)
+        mode = r["mode"]
+        predicted_types = []
+        predicted_candidates = []
+        other_possible_types = []
+        selected_candidates = []
+        mentions = []
+        if mode != "figer":
+            if mode != "custom":
+                selected_inference_processor = InferenceProcessor(mode, resource_loader=self.runner.inference_processor)
+            else:
+                rules = r["taxonomy"]
+                mappings = self.parse_custom_rules(rules)
+                selected_inference_processor = InferenceProcessor(mode, custom_mapping=mappings)
+        else:
+            selected_inference_processor = self.runner.inference_processor
+
+        for sentence in sentences:
+            sentence.set_signature(selected_inference_processor.signature())
+            cached = self.mem_cache.query_cache(sentence)
+            if cached is not None:
+                sentence = cached
+            else:
+                self.runner.process_sentence(sentence, selected_inference_processor)
+                try:
+                    self.mem_cache.insert_cache(sentence)
+                    self.surface_cache.insert_cache(sentence)
+                except:
+                    print("Cache insertion exception. Ignored.")
+            predicted_types.append(list(sentence.predicted_types))
+            predicted_candidates.append(sentence.elmo_candidate_titles)
+            mentions.append(sentence.get_mention_surface_raw())
+            selected_candidates.append(sentence.selected_title)
+            other_possible_types.append(sentence.could_also_be_types)
+
+        elapsed_time = time.time() - start_time
+        print("Processed mention " + str([x.get_mention_surface() for x in sentences]) + " in mode " + mode + ". TIME: " + str(elapsed_time) + " seconds.")
+        ret["type"] = predicted_types
+        ret["candidates"] = predicted_candidates
+        ret["mentions"] = mentions
+        ret["index"] = r["index"]
+        ret["selected_candidates"] = selected_candidates
+        ret["other_possible_type"] = other_possible_types
+        return json.dumps(ret)
+
+    def pipeline_initialize_helper(self, tokens):
+        doc = self.pipeline.doc([tokens], pretokenized=True)
+        doc.get_shallow_parse
+        doc.get_ner_conll
+        doc.get_ner_ontonotes
+        doc.get_view("MENTION")
+
+    def handle_tokenizer_input(self):
+        r = request.get_json()
+        ret = {"tokens": []}
+        if "sentence" not in r:
+            return json.dumps(ret)
+        doc = self.pipeline.doc(r["sentence"])
+        token_view = doc.get_tokens
+        for cons in token_view:
+            ret["tokens"].append(str(cons))
+        return json.dumps(ret)
+
+    """
+    Handles requests for mention filling
+    """
+    def handle_mention_input(self):
+        r = request.get_json()
+        ret = {'mention_spans': []}
+        if "tokens" not in r:
+            return json.dumps(ret)
+        tokens = r["tokens"]
+        doc = self.pipeline.doc([tokens], pretokenized=True)
+        shallow_parse_view = doc.get_shallow_parse
+        ner_conll_view = doc.get_ner_conll
+        ner_ontonotes_view = doc.get_ner_ontonotes
+        md_view = doc.get_view("MENTION")
+        ret_set = set()
+        ret_list = []
+        additions_views = []
+        if ner_ontonotes_view.cons_list is not None:
+            additions_views.append(ner_ontonotes_view)
+        if md_view.cons_list is not None:
+            additions_views.append(md_view)
+        if shallow_parse_view.cons_list is not None:
+            additions_views.append(shallow_parse_view)
+        try:
+            if ner_conll_view.cons_list is not None:
+                for ner_conll in ner_conll_view:
+                    for i in range(ner_conll['start'], ner_conll['end']):
+                        ret_set.add(i)
+                    ret_list.append((ner_conll['start'], ner_conll['end']))
+            for additions_view in additions_views:
+                for cons in additions_view:
+                    add_to_list = True
+                    if additions_view.view_name != "MENTION":
+                        if additions_view.view_name == "SHALLOW_PARSE" and cons['label'] != "NP":
+                            continue
+                        start = int(cons['start'])
+                        end = int(cons['end'])
+                    else:
+                        start = int(cons['properties']['EntityHeadStartSpan'])
+                        end = int(cons['properties']['EntityHeadEndSpan'])
+                    for i in range(max(start - 1, 0), min(len(tokens), end + 1)):
+                        if i in ret_set:
+                            add_to_list = False
+                            break
+                    if add_to_list:
+                        for i in range(start, end):
+                            ret_set.add(i)
+                        ret_list.append((start, end))
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+        ret['mention_spans'] = ret_list
+        return json.dumps(ret)
+
+    """
+    Handles surface form cached requests
+    This is expected to return sooner than actual processing
+    """
+    def handle_simple_input(self):
+        ret = {}
+        r = request.get_json()
+        if "tokens" not in r or "mention_starts" not in r or "mention_ends" not in r or "index" not in r:
+            ret["type"] = [["INVALID_INPUT"]]
+            return json.dumps(ret)
+        sentences = []
+        for i in range(0, len(r["mention_starts"])):
+            sentence = Sentence(r["tokens"], int(r["mention_starts"][i]), int(r["mention_ends"][i]), "")
+            sentences.append(sentence)
+        types = []
+        for sentence in sentences:
+            surface = sentence.get_mention_surface()
+            cached_types = self.surface_cache.query_cache(surface)
+            if cached_types is not None:
+                distinct = set()
+                for t in cached_types:
+                    distinct.add("/" + t.split("/")[1])
+                types.append(list(distinct))
+            else:
+                types.append([])
+        ret["type"] = types
+        ret["index"] = r["index"]
+        return json.dumps(ret)
+
+    def handle_word2vec_input(self):
+        ret = {}
+        r = request.get_json()
+        if "tokens" not in r or "mention_starts" not in r or "mention_ends" not in r or "index" not in r:
+            ret["type"] = [["INVALID_INPUT"]]
+            return json.dumps(ret)
+        sentences = []
+        for i in range(0, len(r["mention_starts"])):
+            sentence = Sentence(r["tokens"], int(r["mention_starts"][i]), int(r["mention_ends"][i]), "")
+            sentences.append(sentence)
+        predicted_types = []
+        for sentence in sentences:
+            self.runner.process_sentence_vec(sentence)
+            predicted_types.append(list(sentence.predicted_types))
+        ret["type"] = predicted_types
+        ret["index"] = r["index"]
+        return json.dumps(ret)
+
+    def handle_elmo_input(self):
+        ret = {}
+        results = []
+        r = request.get_json()
+        if "sentence" not in r:
+            ret["vectors"] = []
+            return json.dumps(ret)
+        elmo_map = self.runner.elmo_processor.process_single_continuous(r["sentence"])
+        for token in r["sentence"].split():
+            results.append((token, str(elmo_map[token])))
+        ret["vectors"] = results
+        return json.dumps(ret)
+
+    """
+    Handler to start the Flask app
+    @localhost: Whether the server lives only in localhost
+    @port: A port number, default to 80 (Web)
+    """
+    def start(self, localhost=False, port=80):
+        self.app.add_url_rule("/", "", self.handle_redirection)
+        self.app.add_url_rule("/<path:path>", "<path:path>", self.handle_root)
+        self.app.add_url_rule("/annotate", "annotate", self.handle_input, methods=['POST'])
+        self.app.add_url_rule("/annotate_token", "annotate_token", self.handle_tokenizer_input, methods=['POST'])
+        self.app.add_url_rule("/annotate_mention", "annotate_mention", self.handle_mention_input, methods=['POST'])
+        self.app.add_url_rule("/annotate_cache", "annotate_cache", self.handle_simple_input, methods=['POST'])
+        self.app.add_url_rule("/annotate_vec", "annotate_vec", self.handle_word2vec_input, methods=['POST'])
+        self.app.add_url_rule("/annotate_elmo", "annotate_elmo", self.handle_elmo_input, methods=['POST'])
+        if localhost:
+            self.app.run()
+        else:
+            self.app.run(host='0.0.0.0', port=port)
+
+    def grace_end(self, signum, frame):
+        print("Gracefully Existing...")
+        if self.runner.elmo_processor.db_loaded:
+            self.runner.elmo_processor.db_conn.close()
+        print("Resource Released. Existing.")
+        exit(0)
+
+
+if __name__ == '__main__':
+    server = Server("/Volumes/External/elmo_cache_correct.db", "./data/surface_cache_new.db")
+    server.start(localhost=True)
+
